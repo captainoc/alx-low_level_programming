a877ae57754f2533731b272993f4ab2457bb53e0
captainoc <icaptainoc@gmail.com>
2022-09-08 18:27:38 -0700

    4. Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,4 +4,5 @@
  * Return: always 0
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }	
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0); }	
