338303a3aab7d6a09932cc22cfdfa6f7568d1083
captainoc <icaptainoc@gmail.com>
2022-09-13 05:44:10 -0700

    1. alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the description
+ *
+ * Description:  prints the alphabet, in lowercase, followed by a new line
+ * Return: 0 if succeaaful
+ */
+int main(void)
+{
+	char i;
+		for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
