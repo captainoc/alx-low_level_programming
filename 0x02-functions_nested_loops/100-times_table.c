04bb289d6a52ef599ba0edb620c215bdb1e4b58b
captainoc <icaptainoc@gmail.com>
2022-09-15 08:57:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,48 @@
 #include "main.h"
 /**
- * print_times_table - times table function
- * @n: integer to return
- *
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-		if (n <= 15 && n >= 0)
-		{
-			for (i = 0; i <= n; i++)
-			{
-				for (j = 0; j <= n; j++)
-				{
-					int prod = j * i;
+	int row;
+	int column;
+	int product;
 
-					if (j == 0)
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
 					{
-						_putchar('0');
-					} else if (prod <= 9)
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
 					{
-						_putchar(',');
 						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + prod);
-					} else if (prod > 99)
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
 					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar('0' + (prod / 100));
-						_putchar('0' + ((prod / 10) % 10));
-						_putchar('0' + (prod % 10));
-					} else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + (prod / 10));
-						_putchar('0' + (prod % 10));
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
 					}
 				}
 			}
+			_putchar('\n');
 		}
+	}
 }
