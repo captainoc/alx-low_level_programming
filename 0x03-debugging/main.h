516f494c95c8a43d75a436227aa995329138e3b4
captainoc <icaptainoc@gmail.com>
2022-09-15 15:38:25 -0700

    1-3

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
